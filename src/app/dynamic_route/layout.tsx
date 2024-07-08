e6457456eeeef035881784c414a08a5c337070d9
Alfredo <21030761@itcelaya.edu.mx>
2024-07-08 13:37:50 -0600

    Rutas dinámicas

---
+++ src/app/dynamic_route/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+      <main>
+        DYNAMIC ROUTE LAYOUT 
+        {children}
+      </main>
+    )
+  }
